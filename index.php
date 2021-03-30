bb123c1c04376bf7d521e7e4cb01b2ab6f565e81
Bryan <bmartinez8493@uta.edu.ec>
2021-03-29 19:10:28 -0500

    Primer Commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript" src="js/jquery35.js"></script>
+    <link rel="StyleSheet" href="css/bootstrap/css/bootstrap.min.css" type="text/css">
+    <script src="css/bootstrap/js/bootstrap.min.js"></script>
+
+    <title>Stream</title>
+</head>
+<body>
+    <table class="table table-dark">
+        <tbody>
+            <tr>
+                <td>aaa</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
