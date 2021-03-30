ade4021f7597d8f31637d28d4e822b327e629f18
Bryan <bmartinez8493@uta.edu.ec>
2021-03-29 19:37:22 -0500

    creacion rama feature1

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,16 +8,15 @@
     <script type="text/javascript" src="js/jquery35.js"></script>
     <link rel="StyleSheet" href="css/bootstrap/css/bootstrap.min.css" type="text/css">
     <script src="css/bootstrap/js/bootstrap.min.js"></script>
-
     <title>Stream</title>
 </head>
+
 <body>
-    <table class="table table-dark">
-        <tbody>
-            <tr>
-                <td>aaa</td>
-            </tr>
-        </tbody>
-    </table>
+
+    <header id="menu">
+    
+    </header>
+
 </body>
+
 </html>
